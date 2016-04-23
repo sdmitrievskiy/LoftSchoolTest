011d4e597ce148d7b15369390abaa9b1465a523f
Sergey Dmitrievskiy <dmitrievskiy-serega@yandex.ru>
2016-04-23 17:18:32 +0300

    Реализовал поведение label при нажатии на него в app.js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,14 @@
-import svg4everybody from 'svg4everybody';
-import $ from 'jquery';
+// import svg4everybody from 'svg4everybody';
+ import $ from 'jquery';
 
-$(() => {
-	svg4everybody();
+console.log(3);
+$(document).ready(function () {
+	console.log(1);
+	//курсов у нас много, поэтому нужно дополнительно реализовать
+	//поведение label
+	//реализуем поведение только в одну строну, ТЗ не требует обратного
+	$('.course__checkbox-label').click(function () {
+		$(this).siblings('.course__checkbox').attr('checked', 'checked');
+	});
+
 });
